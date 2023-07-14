e91c7437a6cd1aa01032156f6d81dac2e7b11f53
JW6661 <121779200+JW6661@users.noreply.github.com>
2023-07-14 16:17:06 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -8,29 +8,68 @@ const gameScore = document.getElementById("game-score")
 const rock = document.getElementById("rock-el")
 const paper = document.getElementById("paper-el")
 const scissors = document.getElementById("scissors-el")
-const choice = ['Rock','Paper','Scissors']
+let choice = ['Rock','Paper','Scissors']
 let playerEl = []
+let computer =  randomChoice ()
 
 savePlayerEl.addEventListener("click", function (){
     playerEl.push(inputEl.value)
     inputEl.value = " "
     renderName()
 })
+ 
+function renderGame(outputOne, outputTwo) {
+    if(outputOne.textContent === 'Rock' && outputTwo.textContent === 'Scissors') {
+        return("You win with rock!");
+    } else if(outputOne.textContent === 'Rock' &&  outputTwo.textContent === 'Paper') {
+        return("You lost with rock!");
+    } else if(outputOne.textContent === 'Paper' &&  outputTwo.textContent === 'Rock') {
+        return("You won with paper!");
+    } else if(outputOne.textContent === 'Paper' &&  outputTwo.textContent === 'Scissors' ) {
+        return("You lost with paper!");
+    } else if(outputOne.textContent === 'Scissors' &&  outputTwo.textContent === 'Paper') {
+        return("You won with sissors!");
+    } else if(outputOne.textContent === 'Scissors' &&  outputTwo.textContent  === 'Rock') {
+        return("You lost with scissors!");
+    } else  (outputOne.textContent === outputTwo.textContent) 
+        return("It's a draw!")
+
+    }
 
 rock.addEventListener("click", function (){
-   outputOne.textContent = choice[0]
-   outputTwo.textContent = randomChoice()
-   
-})
-paper.addEventListener("click", function (){
-    outputOne.textContent = choice[1]
+    outputOne.textContent = choice[0]
     outputTwo.textContent = randomChoice()
+    gameScore.innerHTML = renderGame(outputOne, outputTwo)  
+ 
+   
     
  })
-scissors.addEventListener("click", function (){
-    outputOne.textContent = choice[2]
-    outputTwo.textContent = randomChoice()
- })
+ paper.addEventListener("click", function (){
+     outputOne.textContent = choice[1]
+     outputTwo.textContent = randomChoice()
+     gameScore.innerHTML = renderGame(outputOne, outputTwo)  
+ 
+     
+  })
+ scissors.addEventListener("click", function (){
+     outputOne.textContent = choice[2]
+     outputTwo.textContent = randomChoice ()
+     gameScore.innerHTML = renderGame(outputOne, outputTwo)  
+ 
+  })
+
+    
+function randomChoice (){
+    let randomNumber = Math.floor(Math.random()*3)
+        if (randomNumber === 0) {
+            return 'Rock'
+        } else if (randomNumber === 1) {
+            return 'Paper'
+        } else  {
+            return 'Scissors'
+        } 
+}
+
 
 resetGame.addEventListener("click", function (){
     gameReset()
@@ -40,39 +79,10 @@ function renderName () {
     playerName.innerHTML += playerEl[0]
         if (playerName.innerHTML = playerEl[0])
             return " "
+    
 }
 
-function renderGame(outputOne, outputTwo) {
-        if(outputOne === outputTwo) {
-            console.log("It's a draw!");
-        } else if(outputOne === choice[0] && outputTwo === choice[2]) {
-            console.log("You win with rock!");
-        } else if(outputOne === choice[0] &&  outputTwo === choice[1]) {
-            console.log("You lost with rock!");
-        } else if(outputOne === 1 &&  outputTwo === 0) {
-            console.log("You won with paper!");
-        } else if(outputOne === 1 &&  outputTwo === 2) {
-            console.log("You lost with paper!");
-        } else if(outputOne === 2 &&  outputTwo === 0) {
-            console.log("You won with sissors!");
-        } else if(outputOne === 2 &&  outputTwo === 1) {
-            console.log("You lost with scissors!");
-            }
-        };
 
-
-
-
-function randomChoice (){
-    let randomNumber = Math.floor(Math.random()*3)
-        if (randomNumber === 0) {
-            return choice[0]
-        } else if (randomNumber === 1) {
-            return choice[1]
-        } else  {
-            return choice[2]
-        }
-}
 function gameReset (){
     window.location.reload();
   }
